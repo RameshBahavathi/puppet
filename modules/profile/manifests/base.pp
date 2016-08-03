fe6c7c02339045506d134881b0f5533d34ae9415
RameshBahavathi <Ramesh.baghavathi@gmail.com>
2016-08-03 15:05:00 +0530

    Sensu configuration

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -5,23 +5,28 @@
 #
 class profile::base {
 
-    package { "ntp": 
-        ensure => installed 
+    package { "ntp":
+        ensure => installed
     }
 
     service { "ntp":
         ensure  => running,
         require => Package['ntp']
     }
-	
-	 class { 'sensu':
-     rabbitmq_password  => 'correct-horse-battery-staple',
-     rabbitmq_host      => 'sensu-server.foo.com',
+
+
+    class { 'sensu':
+     rabbitmq_password  => 'secret',
+     rabbitmq_host      => '10.162.52.161',
      subscriptions      => 'sensu-test',
      safe_mode          => true,
+     plugins            => ['file:///etc/puppet/modules/sensu_community_plugins/plugins/system/check-disk.rb','file:///etc/puppet/modules/sensu_community_plugins/plugins/system/check-cpu.rb']
    }
 
+
+
   sensu::check { "diskspace":
-    command => '/etc/sensu/plugins/system/check-disk.rb',
+    command => '/etc/sensu/plugins/check-disk.rb',
   }
+
 }
