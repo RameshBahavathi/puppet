cbc884a0ea0b0d0ed23b0b4657e1fd74882a41b1
RameshBahavathi <Ramesh.baghavathi@gmail.com>
2016-08-09 15:07:52 +0530

    tomcat update

--- modules/tomcat/manifests/config.pp
+++ modules/tomcat/manifests/config.pp
@@ -12,19 +12,4 @@ class tomcat::config {
     content => template( "tomcat/index.html.erb" ),
     notify  => Service['tomcat7']
   }
-
-  file { '/var/lib/tomcat7/webapps/sprint3.war':
-    ensure => present,
-    source  => "puppet:///modules/tomcat/sprint3.war",
-    notify  => Service['tomcat7']
-  }
-
-
- file { 'DBConnection':
-    ensure  => file,
-    path    => '/var/lib/tomcat7/webapps/sprint3/hello.jsp',
-    require => Package['tomcat7'],
-    content => template( "tomcat/hello.jsp.erb" ),
-    notify  => Service['tomcat7']
-  }
 }
