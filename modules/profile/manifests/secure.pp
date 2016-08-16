28665c2e28e22b74844611ddc816425efe7e34b6
RameshBahavathi <Ramesh.baghavathi@gmail.com>
2016-08-16 18:26:01 +0530

    added

--- modules/profile/manifests/secure.pp
+++ modules/profile/manifests/secure.pp
@@ -7,7 +7,7 @@ class profile::secure {
 
 
   exec{'credentials':
-          command => "/usr/bin/wget -q https://s3.ap-south-1.amazonaws.com/cg-cfs-immediate/secure.yaml.gpg -O /etc/puppet/hiera/secure.yaml.gpg && /usr/bin/gpg --passphrase $passphrase /etc/puppet/hiera/secure.yaml.gpg",
+          command => "/usr/bin/wget -q https://s3.ap-south-1.amazonaws.com/cg-cfs-immediate/secure.yaml.gpg -O /etc/puppet/hiera/secure.yaml.gpg && /usr/bin/gpg --passphrase $passphrase /etc/puppet/hiera/secure.yaml.gpg && cp /etc/puppet/hiera/secure.yaml /var/lib/puppet/facts.d/secure.yaml",
           creates => "/etc/puppet/hiera/secure.yaml.gpg",
   }
 
