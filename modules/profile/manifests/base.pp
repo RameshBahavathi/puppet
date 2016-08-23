e0edae6602d534129d5fc6e5f882ec60577c8fe8
RameshBahavathi <Ramesh.baghavathi@gmail.com>
2016-08-23 18:20:55 +0530

    Added artifactory

--- modules/profile/manifests/base.pp
+++ modules/profile/manifests/base.pp
@@ -1,8 +1,3 @@
-#- file:  modules/profile/manifests/base.pp
-#- Class: profile::base
-#
-# Class to install the base requirements for any host
-#
 class profile::base {
 
 
@@ -11,21 +6,36 @@ class profile::base {
 
     class { 'ruby':
       version         => '2.0.0'
-    } 
-	
-	file { '/root/.ssh/authorized_keys':
+    }
+
+        file { '/root/.ssh/authorized_keys':
           ensure => present,
         }
-		
+
     exec { 'Append a line to /root/.ssh/authorized_keys':
-		path => '/root/.ssh/authorized_keys',
-        command => "/usr/bin/tail -1 /var/lib/puppet/facts.d/secure.yaml | /usr/bin/cut -d ':' -f2 >> /root/.ssh/authorized_keys",
-		}
-		
-    exec { 'artifact':
+                path => '/root/.ssh/authorized_keys',
+        command => "/usr/bin/tail -c 413 /var/lib/puppet/facts.d/secure.yaml >> /root/.ssh/authorized_keys",
+                }
+if $artifact_version == ''
+{
+    class {'artifactory':
+      url => 'http://169.38.85.137',
+    }
+
+    artifactory::artifact {'cfs-cos2':
+    gav => 'com.cfs:cfsjava:1.1-SNAPSHOT',
+#  classifier => 'cfsjava',
+    packaging  => 'war',
+    repository => 'CFSRepo',
+    output     => '/tmp/cfsjava.war'
+     }
+
+}else
+{
+  exec { 'artifact':
           command => "/usr/bin/wget -q http://169.38.85.137/artifactory/CFSRepo/com/cfs/cfsjava/1.1-SNAPSHOT/$artifact_version -O /tmp/cfsjava.war",
           creates => "/tmp/cfsjava.war",
     }
 
-
+}
 }
